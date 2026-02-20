6d709758adb85410273c8b742d8860cd7234f628
Maksim Shvetsov <maxik_shvetsov@mail.ru>
2026-02-20 22:15:06 +0800

    first homework numpy_task

---
+++ numpy_task/main.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+a = np.array([3, 2, 1], dtype = "uint8")
+
+assert a.dtype == "uint8" #тип натуральные числа
+
+b = np.array([[1, 2, 3, 4, 5], [-1, -2, -3, -4, -5], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]], dtype = "int8")
+
+assert b.shape == (5, 5) and b.sum() == 0 #матрица 5 на 5 с суммой элементов 0
+
+c = np.array([[[1]]])
+
+assert c.ndim == 3 and c.sum() / c.size == 1 #глебина 3, сумма/число элементов = 1
+
+d = np.arange(-5, 5, 1) #генератор
+
+assert np.all(d == np.array([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4])) 
+
+e = np.arange(0, 1.25, 0.25) #генератор дробный
+
+assert np.all(e == np.array([0., 0.25, 0.5, 0.75, 1.0]))
+
+f = np.arange(5 * 5).reshape(5, 5)
+fc = np.arange(0, 30, 10).reshape(3, 1)+np.arange(1, 4, 2) #это единственный номер, за которым я полез просить совета нейронку
+#оказалось, сложение одномерных вертикального и горизонтальных массивов их перемножит
+assert np.all(fc == np.array([[1, 3], [11, 13], [21, 23]]))
+
+g = np.ones((5, 3))
+gc = np.ones(5)*3 #умножение на число
+
+assert np.all(gc == np.array([3., 3., 3., 3., 3.]))
+
+h = np.arange(5) + 1
+hc = np.arange(0, 10, 2) + 2 #прибавление числа
+
+assert np.all(hc == np.array([2., 4., 6., 8., 10.]))
+
+j = np.array([1, 2, 3, 4, 9, 7, 11, 12, 15, 14, 33])
+mask = j % 3 == 0
+jc = j[mask]
+
+assert np.all(jc == np.array([3, 9, 12, 15, 33])) #кратные трём
+
+k = np.array([1, 2, 3, 4, 5])
+l = np.array([2, 2, 3, 3, 4])
+kl = k**l #один массив - стенени другого
+
+assert np.all(kl == np.array([1, 4, 27, 64, 625]))
+
+m = np.array([2, 2, 2, 3, 3, 3])
+mc = np.std(m) #отклонение
+
+assert mc == 0.5
+
+n = np.array([1, 2, 3, 4, 5, 6])
+nc = np.mean(n) #среднее
+
+assert nc == 3.5
+
+o = np.array([2, 2, 2, 2])
+oc = o.reshape(2,2) #приведение к квадрату
+
+assert oc.ndim == 2 and oc.shape == (2, 2)
+
+p = np.array([1, 2, 3, 4])
+pc = 5 - p #вычитание из числа
+
+assert np.all(pc == np.array([4, 3, 2, 1]))
+
+r = np.array([3, 3, 5, 5])
+rc = np.ones(4) - 2
+rc[1:3] = -1 #срез не изменяет сам массив
+
+assert np.all(r[1:3] == np.array([3, 5])) and np.all(rc[1:3] == np.array([-1, -1]))
