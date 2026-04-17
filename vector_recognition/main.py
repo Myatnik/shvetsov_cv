b74b777c9e26926992cc7dc8148f2ea862357cab
Maksim Shvetsov <maxik_shvetsov@mail.ru>
2026-04-17 22:03:07 +0800

    finished first symbol recognition homework

--- vector_recognition/main.py
+++ vector_recognition/main.py
@@ -6,8 +6,63 @@ from pathlib import Path
 
 save_path = Path(__file__).parent
 
+def count_holes(region):
+    shape = region.image.shape
+    new_image = np.zeros((shape[0] + 2, shape[1] + 2))
+    new_image[1:-1, 1:-1] = region.image
+    new_image = np.logical_not(new_image)
+    labeled = label(new_image)
+    return np.max(labeled) - 1
+
+def count_lines(region):
+    shape = region.image.shape
+    image = region.image
+    vlines = (np.sum(image, 0) / shape[0] == 1).sum()
+    hlines = (np.sum(image, 1) / shape[1] == 1).sum()
+    return vlines, hlines
+
+def symmetry(region, transpose=False):
+    image = region.image
+    if transpose:
+        image = image.T
+    shape = image.shape
+    top = image[:shape[0] // 2]
+    if shape[0] % 2 != 0:
+        bottom = image[shape[0] // 2 + 1:]
+    else:
+        bottom = image[shape[0] // 2:]
+    bottom = bottom[::-1]
+    result = bottom == top
+    return result.sum() / result.size
+
 def extractor(region):
-    return np.array([region.area/region.image.size])
+    cy, cx = region.centroid_local
+    cy /= region.image.shape[0]
+    cx /= region.image.shape[1]
+    perimeter = region.perimeter / region.image.size
+    holes = count_holes(region)
+    v, h = count_lines(region)
+    v /= region.image.shape[1]
+    h /= region.image.shape[0]
+    eccentricity = region.eccentricity
+    aspect = region.image.shape[0] / region.image.shape[1]
+    solidity = region.solidity
+    extent = region.extent
+    v_sym = symmetry(region)
+    h_sym = symmetry(region, transpose=True)
+    return np.array([
+        region.area/region.image.size,
+        cx, cy,
+        perimeter,
+        holes,
+        v, h,
+        eccentricity,
+        aspect,
+        solidity,
+        extent,
+        v_sym,
+        h_sym
+        ])
 
 def classificator(region, templates):
     features = extractor(region)
@@ -19,6 +74,7 @@ def classificator(region, templates):
             result = symbol
             min_d = d
     return result
+
 template = imread("alphabet-small.png")[:, :, :-1]
 template = template.sum(2)
 binary = template != 765.
@@ -36,8 +92,8 @@ for region, symbol in zip(props, ["8", "O", "A", "B", "1", "W", "X", "*", "/", "
 
 
 image = imread("alphabet.png")[:, :, :-1]
-binary_alphabet = image.mean(2) > 0
-alabeled = label(binary_alphabet)
+abinary = image.mean(2) > 0
+alabeled = label(abinary)
 aprops = regionprops(alabeled)
 result = {}
 image_path = save_path/"out"
